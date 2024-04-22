2f6952f0a8d22bed331cbaed20b645272f3ee6a3
Jason Ky <744169@student.dusd.net>
2024-04-22 15:07:18 -0700

    yayaya
    - working on movement

--- Lab2ProjectKy7/Assets/Scripts/PlayerController.cs
+++ Lab2ProjectKy7/Assets/Scripts/PlayerController.cs
@@ -8,21 +8,28 @@ public class PlayerController : MonoBehaviour
     public float speed = 20.0f;
     public float health = 100.0f;
 
-    float horizontal;
-    float vertical;
+
+    Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * speed * Time.deltaTime * horizontalInput);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
         }
     }
 }
